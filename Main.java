b82e38568e3c37f1c2ca792fdf2bc19ca20536c4
sijan <sijan@localhost.localdomain>
2023-06-12 11:01:59 +0545

    first commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException {
+        Login login = new Login();
+        login.loginView();
+
+
+    }
+}
+
+
+
